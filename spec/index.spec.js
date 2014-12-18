4d15ba6945620ce93edfe08c5b355aba29c7af56
timkim <timk@adobe.com>
2014-12-17 16:26:45 -0800

    tab to spaces

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -12,7 +12,7 @@ var cordova = require('./helper/cordova'),
 describe('phonegap-plugin-contentsync', function() {
     describe('.sync', function() {
         it('should return an instance of ContentSync', function() {
-			var options = { src:'dummySrc' };
+            var options = { src:'dummySrc' };
             var sync = content.sync(options);
 
             expect(sync).toEqual(jasmine.any(content.ContentSync));
