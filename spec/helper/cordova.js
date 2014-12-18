d8c57a724e1c6311d61d0c3049fb2985a47a4c99
Michael Brooks <michael@michaelbrooks.ca>
2014-12-18 14:31:51 -0600

    [#3] Add ability to spy on cordova.require modules.

--- spec/helper/cordova.js
+++ spec/helper/cordova.js
@@ -5,25 +5,79 @@
 var path = require('path');
 
 /**
- * Cordova Require.
+ * cordova.js for node.
  *
- * Loads a cordova module by mocking the cordova require
- * implementation and returning the cordova module as a
- * node module.
+ * Think of this as cordova-node, which would be simliar to cordova-android
+ * or cordova-browser. The purpose of this module is to enable testing
+ * of a plugin's JavaScript interface.
  *
- * @param {String} modulePath is the path to the cordova module.
- * @return {Object} that represents the cordova module.
+ * When this module is first required, it will insert a global cordova
+ * instance, which can hijack cordova-specific commands within the pluin's
+ * implementation.
+ *
+ * Remember to require this module before the plugin that you want to test.
+ *
+ * Example:
+ *
+ *     var cordova = require('./helper/cordova'),
+ *         myPlugin = require('../www/myPlugin');
  */
 
-module.exports.require = function(modulePath) {
-    modulePath = path.resolve(modulePath) + '.js';
+module.exports = global.cordova = cordova = {
 
-    if (!global.cordova) {
-        global.cordova = {
-            require: function() {
-            }
-        };
+    /**
+     * cordova.require Mock.
+     *
+     * Hijacks all cordova.requires. By default, it returns an empty function.
+     * You can define your own implementation of each required module before
+     * or after it has been required.
+     *
+     * See `cordova.required` to learn how to add your own module implemtnation.
+     */
+
+    require: function(moduleId) {
+        // define a default function if it doesn't exist
+        if (!cordova.required[moduleId]) {
+            cordova.required[moduleId] = function() {};
+        }
+        // create a new module mapping between the module Id and cordova.required.
+        return new ModuleMap(moduleId);
+    },
+
+    /**
+     * Cordova module implementations.
+     *
+     * A key-value hash, where the key is the module such as 'cordova/exec'
+     * and the value is the function or object returned.
+     *
+     * For example:
+     *
+     *     var exec = require('cordova/exec');
+     *
+     * Will map to:
+     *
+     *     cordova.required['cordova/exec'];
+     */
+
+    required: {
+        // populated at runtime
     }
-
-    return require(modulePath);
 };
+
+/**
+ * Module Mapper.
+ *
+ * Returns a function that when executed will lookup the implementation
+ * in cordova.required[id].
+ *
+ * @param {String} moduleId is the module name/path, such as 'cordova/exec'
+ * @return {Function}.
+ */
+
+function ModuleMap(moduleId) {
+    return function() {
+        // lookup and execute the module's mock implementation, passing
+        // in any parameters that were provided.
+        return cordova.required[moduleId].apply(this, arguments);
+    };
+}
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,19 +3,37 @@
  */
 
 var cordova = require('./helper/cordova'),
-    content = cordova.require('./www/sync');
+    contentSync = require('../www/sync'),
+    execSpy;
 
 /*!
  * Sync specification.
  */
 
 describe('phonegap-plugin-contentsync', function() {
+    beforeEach(function() {
+        // spy on any cordova.required module just like any other jasmine spy.
+        execSpy = spyOn(cordova.required, 'cordova/exec');
+    });
+
     describe('.sync', function() {
         it('should return an instance of ContentSync', function() {
-            var options = { src:'dummySrc' };
-            var sync = content.sync(options);
+            var sync = contentSync.sync({ src: 'dummySrc' });
+            expect(sync).toEqual(jasmine.any(contentSync.ContentSync));
+        });
 
-            expect(sync).toEqual(jasmine.any(content.ContentSync));
+        it('should delegate to exec', function() {
+            var sync = contentSync.sync({ src: 'dummySrc' });
+            // simple
+            expect(execSpy).toHaveBeenCalled();
+            // detailed
+            expect(execSpy).toHaveBeenCalledWith(
+                jasmine.any(Function),
+                null,
+                'Sync',
+                'sync',
+                jasmine.any(Object)
+            );
         });
     });
 });
