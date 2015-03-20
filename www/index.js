f44f5510535f27b87b2b9343cdd68d79b77812cc
Anis Kadri <anis@apache.org>
2015-03-20 16:32:16 -0700

    adding copyCordovaAssets to exec options as last argument

--- www/index.js
+++ www/index.js
@@ -79,7 +79,7 @@ var ContentSync = function(options) {
 
     // wait at least one process tick to allow event subscriptions
     setTimeout(function() {
-        exec(success, fail, 'Sync', 'sync', [options.src, options.id, options.type, options.headers]);
+        exec(success, fail, 'Sync', 'sync', [options.src, options.id, options.type, options.headers, options.copyCordovaAssets]);
     }, 10);
 };
 
